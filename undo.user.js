797ed14d0589ec827194feba2a10917d340de35b
Markus-Rost <37275477+Markus-Rost@users.noreply.github.com>
2018-08-31 02:32:23 +0200

    Add files via upload

---
+++ undo.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name GRASP undo
+// @version 1.0
+// @description Undo with link to GRASP
+// @author MarkusRost
+// @updateURL https://github.com/Markus-Rost/GRASP/raw/master/undo.user.js
+// @downloadURL https://github.com/Markus-Rost/GRASP/raw/master/undo.user.js
+// @include https://*.gamepedia.com/*
+// @grant none
+// ==/UserScript==
+
+var $Summary = $( '#wpSummary input[type=text],#wpSummary[type=text]' );
+if ( $('.mw-undo-success').length ) $Summary.val( '[[gphelp:GRASP|GRASP]]: ' + $Summary.val() );
