eb278e7f9d647789fc036bf0f2c5ab156c65d6b2
Markus Rost <37275477+Markus-Rost@users.noreply.github.com>
2019-02-19 19:54:40 +0100

    Update fastDelete.user.js

--- fastDelete.user.js
+++ fastDelete.user.js
@@ -66,7 +66,7 @@ mw.loader.using(['site','mediawiki.util']).done(function() {
 
     $( '#right-navigation' ).on( 'click', '.fast-delete-submit-button', function() {
 		new mw.Api().postWithToken('csrf',{action:"delete",title:mw.config.get("wgPageName"),reason:$( '.fast-delete-text' ).val(),watchlist:"nochange"}).done(function(data){
-			window.location = '/' + mw.config.get("wgPageName");
+			window.location = '/' + data.delete.title;
 		}).fail(function(code, data){
 			alert("Could not delete this page. Reason: " + code);
             location.reload();
