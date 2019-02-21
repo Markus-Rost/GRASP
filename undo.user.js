250b56207841c2d3fbc72012aeadcdba1d1a381d
Markus Rost <37275477+Markus-Rost@users.noreply.github.com>
2019-02-21 01:54:47 +0100

    Update undo.user.js

--- undo.user.js
+++ undo.user.js
@@ -6,6 +6,7 @@
 // @updateURL https://github.com/Markus-Rost/GRASP/raw/master/undo.user.js
 // @downloadURL https://github.com/Markus-Rost/GRASP/raw/master/undo.user.js
 // @include https://*.gamepedia.com/*
+// @require https://help.gamepedia.com/load.php?modules=jquery&only=scripts
 // @grant none
 // ==/UserScript==
 
