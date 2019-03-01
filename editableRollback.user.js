3f204d61b9d3359f6426a49896cb1319a47f8dfa
Markus Rost <37275477+Markus-Rost@users.noreply.github.com>
2019-03-01 23:01:10 +0100

    Update editableRollback.user.js

--- editableRollback.user.js
+++ editableRollback.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name GRASP editableRollback
-// @version 1.1
+// @version 1.2
 // @description Edit rollback summary with link to GRASP
 // @author MarkusRost
 // @updateURL https://github.com/Markus-Rost/GRASP/raw/master/editableRollback.user.js
 // @downloadURL https://github.com/Markus-Rost/GRASP/raw/master/editableRollback.user.js
 // @include https://*.gamepedia.com/*
+// @exclude https://www.gamepedia.com/*
 // @require https://help.gamepedia.com/load.php?modules=jquery&only=scripts
 // @run-at document-idle
 // @grant GM_addStyle
